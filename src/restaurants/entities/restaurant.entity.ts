e4a2b9b99995ca0a1f5620835070d7484484ecc0
maintainker <syhwan88@naver.com>
2021-02-12 20:01:15 +0900

    [learning] #1 graphql cloning

---
+++ src/restaurants/entities/restaurant.entity.ts
@@ -0,0 +1,9 @@
+import { ObjectType, Field } from '@nestjs/graphql';
+
+@ObjectType()
+export class Restaurant {
+  @Field(() => String)
+  name: string;
+  @Field(() => Boolean, { nullable: true })
+  isGood?: boolean;
+}
---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,10 @@
+import { Resolver, Query, Args } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
+
+@Resolver(() => Restaurant)
+export class RestaurantResolver {
+  @Query(() => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
+  }
+}
